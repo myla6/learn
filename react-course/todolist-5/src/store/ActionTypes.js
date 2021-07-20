f15ded47b82d57692f29ec8f17ca5c2d1a61a8cc
李红梅 <lihm@seeyon.com>
2021-07-20 18:11:40 +0800

    delete:删除todolist

--- react-course/todolist-5/src/store/ActionTypes.js
+++
@@ -1,7 +0,0 @@
-//解决字符串书写错误不显示报错信息的问题
-//常量或者变量会提示报错信息
-
-export const CHANGE_INPUT_VALUE ='change_input_value';
-export const ADD_TODO_ITEM='add_todo_item';
-export const DELETE_TODO_ITEM='delete_todo_item';
-
--- react-course/todolist-6/README.md
+++
@@ -1,9 +0,0 @@
-$ npm install antd --save
-
-
-
-redux设计和使用的原则
-
-1.store是唯一的
-2.只有store能够改变自己的内容
-3.reducer 是纯函数
